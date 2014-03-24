75bf998810c3e552882f65667f9b8779ab8c223b
Ronak Patel <patel66@sbcglobal.net>
2014-03-24 21:57:33 +0000

    Describe .max finished

--- arrays.rb
+++ arrays.rb
@@ -3,13 +3,34 @@ require 'pry-debugger'
 
 module ArrayUtil
   def self.max(array)
-    check = 0
+    @tester = array[0]
+
     if array.length == 0
       return nil
+    elsif array.length == 1
+      return array[0]
+    #elsif array.length == 2
+     # if array[1] > array[0]
+      #  return array[1]
+     # else
+     #   return array[0]
+     #end
+    else
+      array.each do |num|
+        if num >= @tester
+          @tester = num
+        end
+      end
+      return @tester
+
+
     end
   end
 
   def self.middle_element(array)
+
+
+
   end
 
   def self.sum_arrays(array1, array2)
