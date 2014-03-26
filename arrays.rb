1f1de2402a53aaaad649b628d478d522a9b37138
Antonio Felguerez <agfelgue@gmail.com>
2014-03-26 07:38:45 -0500

    Adds self.middle_element method to pass middle_element tests:
    .middle_element returns nil for an empty array
    .middle_element returns the only element for an array.length == 1
    .middle_element averages 2 middle elements for even-sized arrays
    .middle_element returns floats when the middle element avereages a decimal
    
    Also adds self.sum_arrays method that:
    
    returns an empty array if both inputs are empty
    returns a single value array when sum'ing two single value arrays

--- arrays.rb
+++ arrays.rb
@@ -3,11 +3,32 @@ require 'pry-debugger'
 
 module ArrayUtil
   def self.max(array)
-  end
+  	max = array[0]
+	  	array.each do |element|
+	  		if element > max
+	  			max = element
+	  		end
+	  	end
+	  	return max
+  	end
 
   def self.middle_element(array)
+  	middle_element = array[0]
+  	if array.length == 0
+  		return nil
+  	elsif array.length % 2 == 1
+  		return array[array.length/2]
+  	else
+  		halfway = array.length/2.0
+  		return (array[halfway] + array[halfway - 1])/2.0
+  	end
   end
 
   def self.sum_arrays(array1, array2)
+    if array1 == [] && array2 == []
+      return []
+    else # array1.length == 1 && array1.length == 1
+      return [array1[0] + array2[0]]
+    end
   end
 end
