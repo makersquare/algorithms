46045b43739a6593af6a69c85652ebff48223f1f
Greg Cosgrove <gregcosgrove@gmail.com>
2014-04-27 14:49:19 -0500

    ArrayUtil.mx returns nil

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -3,11 +3,19 @@ require 'pry-debugger'
 
 module ArrayUtil
   def self.max(array)
+
   end
+
+
+
 
   def self.middle_element(array)
   end
 
   def self.sum_arrays(array1, array2)
+
+
   end
 end
+
+
--- array_problems/spec/arrays_spec.rb
+++ array_problems/spec/arrays_spec.rb
@@ -15,62 +15,62 @@ describe "ArrayUtil" do
       expect(ArrayUtil.max([1])).to eq(1)
     end
 
-    it "should return the larger element if the array has 2 elements" do
+    xit "should return the larger element if the array has 2 elements" do
       expect(ArrayUtil.max([-1, 1])).to eq(1)
       expect(ArrayUtil.max([0, -10])).to eq(0)
       expect(ArrayUtil.max([1, 10])).to eq(10)
     end
 
-    it "should find the largest number in a large array" do
+    xit "should find the largest number in a large array" do
       expect(ArrayUtil.max([100, 50, 200, 73, 4.2, 80])).to eq(200)
     end
 
-    it "should not break when you have duplicates" do
+    xit "should not break when you have duplicates" do
       expect(ArrayUtil.max([10, 10])).to eq(10)
       expect(ArrayUtil.max([1, 10, 10])).to eq(10)
       expect(ArrayUtil.max([10, 10, 20])).to eq(20)
     end
 
-    it "should not break when you have negative numbers" do
+    xit "should not break when you have negative numbers" do
       expect(ArrayUtil.max([-1, -5, -10, -2])).to eq(-1)
     end
   end
 
   describe ".middle_element" do
-    it "should return nil for an empty array" do
+    xit "should return nil for an empty array" do
       expect(ArrayUtil.middle_element([])).to eq(nil)
     end
 
-    it "should return the only element for array of size 1" do
+    xit "should return the only element for array of size 1" do
       expect(ArrayUtil.middle_element([1])).to eq(1)
     end
 
-    it "should return the middle element for arrays of odd size" do
+    xit "should return the middle element for arrays of odd size" do
       expect(ArrayUtil.middle_element([1])).to eq(1)
       expect(ArrayUtil.middle_element([1, 2, 3])).to eq(2)
       expect(ArrayUtil.middle_element([1, 3, 5, 7, 9])).to eq(5)
     end
 
-    it "should average the 2 middle elements for arrays of even size" do
+    xit "should average the 2 middle elements for arrays of even size" do
       expect(ArrayUtil.middle_element([1, 3])).to eq(2)
       expect(ArrayUtil.middle_element([1, 3, 5, 7, 9, 11])).to eq(6)
     end
-   
-    it "should return floats when the middle elements average to a decimal" do
+
+    xit "should return floats when the middle elements average to a decimal" do
       expect(ArrayUtil.middle_element([1, 2, 3, 4])).to eq(2.5)
     end
   end
 
   describe ".sum_arrays" do
-    it "should return an empty array if the inputs are empty" do
+    xit "should return an empty array if the inputs are empty" do
       expect(ArrayUtil.sum_arrays([], [])).to eq([])
     end
 
-    it "should return an array with the sum of two arrays with arrays size 1" do
+    xit "should return an array with the sum of two arrays with arrays size 1" do
       expect(ArrayUtil.sum_arrays([5], [6])).to eq([11])
     end
 
-    it "should return an array with the sum of two arrays with larger arrays" do
+    xit "should return an array with the sum of two arrays with larger arrays" do
       expect(ArrayUtil.sum_arrays([5, 6, 7, 8], [6, 9, 12, 15])).to eq([11, 15, 19, 23])
     end
   end
