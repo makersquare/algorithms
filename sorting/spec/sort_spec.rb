5e1cf8d1d21c451685e986060ef6791c6128b1cb
Bobby Smith <bobbysmith24@gmail.com>
2014-05-07 14:00:45 -0500

    add sort method

--- sorting/spec/sort_spec.rb
+++ sorting/spec/sort_spec.rb
@@ -49,52 +49,96 @@ describe 'Sort' do
 
   end
 
-  describe ".merge" do
+    describe ".sort" do
 
     context 'when the array is empty' do
       it "returns an empty array" do
-        expect(Sort.merge([], [])).to eq([])
+        expect(Sort.sort([])).to eq([])
       end
     end
 
     context 'when an array contains a negative integer' do
       it 'returns a sorted array' do
-        expect(Sort.merge([5, 2, 9, 11], [4, 7, 6, -2])).to eq([-2, 2, 4, 5, 6, 7, 9, 11])
+        expect(Sort.sort([5, 2, 9, 11, 4, 7, 6, -2])).to eq([[2, 5, 9, 11], [-2, 4, 6, 7]])
       end
     end
 
     context 'when an array contains duplicate integers' do
       it 'returns a sorted array' do
-        expect(Sort.merge([7, 5, 9], [3, 4, 1, 3])).to eq([1, 3, 3, 4, 5, 7, 9])
+        expect(Sort.sort([7, 5, 9, 3, 4, 1, 3])).to eq([[5, 7, 9], [1, 3, 3, 4]])
       end
     end
 
-    context 'when an array contains only negative integers' do
-      it 'returns an array of sorted negtive integers' do
-        expect(Sort.merge([-2, -3, -9], [-4, -11])). to eq([-11, -9, -4, -3, -2])
-      end
-    end
+    # context 'when an array contains only negative integers' do
+    #   it 'returns an array of sorted negtive integers' do
+    #     expect(Sort.sort([-2, -3, -9], [-4, -11])). to eq([-11, -9, -4, -3, -2])
+    #   end
+    # end
 
-    context 'when an array contains one element' do
-      it 'return the original array' do
-        expect(Sort.merge([7], [4])).to eq([4, 7])
-      end
-    end
+    # context 'when an array contains one element' do
+    #   it 'return the original array' do
+    #     expect(Sort.sort([7], [4])).to eq([4, 7])
+    #   end
+    # end
 
-    context 'when an array contains floats' do
-      it 'returns a sorted array' do
-        expect(Sort.merge([2, 7, 5.5], [3, 1, -3, 2.2])).to eq([-3, 1, 2, 2.2, 3, 5.5, 7])
-      end
-    end
+    # context 'when an array contains floats' do
+    #   it 'returns a sorted array' do
+    #     expect(Sort.sort([2, 7, 5.5], [3, 1, -3, 2.2])).to eq([-3, 1, 2, 2.2, 3, 5.5, 7])
+    #   end
+    # end
 
-    context 'when an array contains 0' do
-      it 'retrurns a sorted array' do
-        expect(Sort.merge([0, 12, 23], [3, 5, 6, 0, 1])).to eq([0, 0, 1, 3, 5, 6, 12, 23])
-      end
-    end
-
-
+    # context 'when an array contains 0' do
+    #   it 'retrurns a sorted array' do
+    #     expect(Sort.sort([0, 12, 23], [3, 5, 6, 0, 1])).to eq([0, 0, 1, 3, 5, 6, 12, 23])
+    #   end
+    # end
 
   end
+
+  # describe ".merge" do
+
+  #   context 'when the array is empty' do
+  #     it "returns an empty array" do
+  #       expect(Sort.merge([], [])).to eq([])
+  #     end
+  #   end
+
+  #   context 'when an array contains a negative integer' do
+  #     it 'returns a sorted array' do
+  #       expect(Sort.merge([5, 2, 9, 11], [4, 7, 6, -2])).to eq([-2, 2, 4, 5, 6, 7, 9, 11])
+  #     end
+  #   end
+
+  #   context 'when an array contains duplicate integers' do
+  #     it 'returns a sorted array' do
+  #       expect(Sort.merge([7, 5, 9], [3, 4, 1, 3])).to eq([1, 3, 3, 4, 5, 7, 9])
+  #     end
+  #   end
+
+  #   context 'when an array contains only negative integers' do
+  #     it 'returns an array of sorted negtive integers' do
+  #       expect(Sort.merge([-2, -3, -9], [-4, -11])). to eq([-11, -9, -4, -3, -2])
+  #     end
+  #   end
+
+  #   context 'when an array contains one element' do
+  #     it 'return the original array' do
+  #       expect(Sort.merge([7], [4])).to eq([4, 7])
+  #     end
+  #   end
+
+  #   context 'when an array contains floats' do
+  #     it 'returns a sorted array' do
+  #       expect(Sort.merge([2, 7, 5.5], [3, 1, -3, 2.2])).to eq([-3, 1, 2, 2.2, 3, 5.5, 7])
+  #     end
+  #   end
+
+  #   context 'when an array contains 0' do
+  #     it 'retrurns a sorted array' do
+  #       expect(Sort.merge([0, 12, 23], [3, 5, 6, 0, 1])).to eq([0, 0, 1, 3, 5, 6, 12, 23])
+  #     end
+  #   end
+
+  # end
 
 end
