0454d4f9ddfcdba1af7d7a73aaf48029a80df548
Bobby Smith <bobbysmith24@gmail.com>
2014-05-09 13:37:36 -0500

    hash algorithm

--- my_hash/my_hash.rb
+++ my_hash/my_hash.rb
@@ -1,13 +1,26 @@
 class MyHash
   def initialize
-  end
-
-  def find_index(key)
+    @pairs = Array.new(4) {[]}
   end
 
   def []=(key, value)
+    index = find_index(key)
+    @pairs[index].each do |pair|
+      pair[-1] = value if pair.first == key
+    end
+    @pairs[index] << [key, value]
   end
 
   def [](key)
+    index = find_index(key)
+    @pairs[index].each do |pair|
+      return pair[-1] if pair.first == key
+    end
+      return nil
   end
+
+  def find_index(key)
+    key.hash % 4
+  end
+
 end
