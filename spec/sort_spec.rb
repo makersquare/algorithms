da76dda71490ecc94bc66a00cf5efd726834d84c
Catherine O'Neill <cathzozo@gmail.com>
2014-08-04 09:29:38 -0500

    test for duplicates

--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -11,10 +11,18 @@ describe 'Sort' do
       end
     end
 
-    context 'when the argument is empty' do
+    context 'when the array includes negative numbers' do
       it "correctly sorts negative numbers" do
         array = [6, 9, 8, -2, 100, 0, -44]
         result = [-44, -2, 0, 6, 8, 9, 100]
+        expect(Sort.selection_sort(array)).to eq(result)
+      end
+    end
+
+    context 'when the array includes duplicate numbers' do
+      it "correctly sorts duplicate numbers" do
+        array = [6, 9, 9, -2, 100, 0, 0]
+        result = [-2, 0, 0, 6, 9, 9, 100]
         expect(Sort.selection_sort(array)).to eq(result)
       end
     end
