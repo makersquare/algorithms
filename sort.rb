2c84a38a2e603bd08045ecb2ebbe7ef4039a90db
Catherine O'Neill <cathzozo@gmail.com>
2014-08-04 10:15:25 -0500

    combined lines for readability

--- sort.rb
+++ sort.rb
@@ -5,14 +5,12 @@ module Sort
     last = array.length - 1
     for i in 0..last-1
       return nil if !array[i].is_a?(Numeric)
-      min_value = array[i]
-      min_index = i
+      min_value, min_index = array[i], i
       #iterate through array to find smallest value -- start with index value to end
       for j in i+1..last 
         return nil if !array[j].is_a?(Numeric)
         if array[j] < min_value
-          min_value = array[j]
-          min_index = j
+          min_value, min_index = array[j], j
         end
       end
       #switch it for the index value
