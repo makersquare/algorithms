3e1f9225a6c7651b489124203d4f8678b8d84bc4
Catherine O'Neill <cathzozo@gmail.com>
2014-08-19 17:16:01 -0500

    imperfect queue

--- stack_queue.rb
+++ stack_queue.rb
@@ -13,3 +13,35 @@ class Stack
     @stack[-1]
   end
 end
+
+class Queue
+  attr_accessor :next_node
+  @@first_in_line = nil
+  @@last_in_line = nil
+
+  def initialize(value)
+    @value = value
+    @next_node = nil
+    @@last_in_line.next_node = self unless @@last_in_line.nil?
+    @@last_in_line = self
+    @@first_in_line = self if @@first_in_line.nil?
+  end
+
+
+  def self.push(value)
+    Queue.new(value)
+  end
+
+  def self.shift
+    first = @@first_in_line
+    second = @@first_in_line.next_node
+    @@first_in_line.next_node = nil
+    if second.nil?
+      @@first_in_line, @@last_in_line = nil, nil
+    else
+      @@first_in_line = second
+    end
+    first
+  end
+end
+
