3e26416e4f4dc43e020a3f31b2a91cc1f964127c
Catherine O'Neill <cathzozo@gmail.com>
2014-08-19 13:42:49 -0500

    rspec testing for mergesort

--- sort.rb
+++ sort.rb
@@ -39,40 +39,40 @@ module Sort
 
   def self.mergesort(arr)
     return arr if arr.length == 1
+    return [] if arr.empty?
     mid = arr.length/2
     array1 = Sort.mergesort(arr[0..mid-1])
     array2 = Sort.mergesort(arr[mid..-1])
-
-    def merge(array1, array2)
-      a = 0
-      b = 0
-      sorted_array = []
-
-      while (a < array1.length && b < array2.length)
-        if array1[a] < array2[b] 
-          sorted_array.push(array1[a])
-          a += 1
-        else
-          sorted_array.push(array2[b])
-          b += 1
-        end
-      end
-
-      while (a < array1.length)
-          sorted_array.push(array1[a])
-          a += 1  
-      end
-
-      while (b < array2.length)
-          sorted_array.push(array2[b])
-          b += 1  
-      end
-
-      sorted_array
-    end
-    
-    merge(array1, array2)
+    Sort.merge(array1, array2)
   end
+
+  def self.merge(array1, array2) #helper method for Merge Sort
+    a = 0
+    b = 0
+    sorted_array = []
+
+    while (a < array1.length && b < array2.length)        
+      if array1[a] < array2[b] 
+        sorted_array.push(array1[a])
+        a += 1
+      else
+        sorted_array.push(array2[b])
+        b += 1
+      end
+    end
+
+    while (a < array1.length)
+        sorted_array.push(array1[a])
+        a += 1  
+    end
+
+    while (b < array2.length)
+        sorted_array.push(array2[b])
+        b += 1  
+    end
+    sorted_array
+  end    
+
 
   def self.binary_search(sorted_array, search_element)
     mid = sorted_array.length/2
--- spec/sort_spec.rb
+++ spec/sort_spec.rb
@@ -100,4 +100,52 @@ describe 'Sort' do
       end
     end
   end
+
+  describe '.mergesort' do
+    context 'when the argument is empty' do
+      it "returns an empty array" do
+        expect(Sort.mergesort([])).to eq([])
+      end
+    end
+
+    context 'when the array includes negative numbers' do
+      it "correctly sorts negative numbers" do
+        array = [6, 9, 8, -2, 100, 0, -44]
+        result = [-44, -2, 0, 6, 8, 9, 100]
+        expect(Sort.mergesort(array)).to eq(result)
+      end
+    end
+
+    context 'when the array includes duplicate numbers' do
+      it "correctly sorts duplicate numbers" do
+        array = [6, 9, 9, -2, 100, 0, 0]
+        result = [-2, 0, 0, 6, 9, 9, 100]
+        expect(Sort.mergesort(array)).to eq(result)
+      end
+    end
+
+    context 'when the array is large' do
+      it "correctly sorts arrays of any size" do
+        array = [6, 9, 8, -2, 100, 0, -44, 4, 5, 8, 1000, -900, -50, 75, 75, 99, 1000]
+        result = [-900, -50, -44, -2, 0, 4, 5, 6, 8, 8, 9, 75, 75, 99, 100, 1000, 1000]
+        expect(Sort.mergesort(array)).to eq(result)
+      end
+    end
+
+    context 'when the array includes decimals' do
+      it "correctly sorts decimals" do
+        array = [6, 9, 8, -2, 100, 0, 8.5, 42.42]
+        result = [-2, 0, 6, 8, 8.5, 9, 42.42, 100]
+        expect(Sort.mergesort(array)).to eq(result)
+      end
+    end
+
+    context 'when non-numbers are included' do
+      xit "returns nil" do
+        array = [6, 9, 8, -2, "dog", 0, -44]
+        result = nil
+        expect(Sort.mergesort(array)).to eq(result)
+      end
+    end
+  end
 end
