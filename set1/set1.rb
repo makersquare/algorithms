541e8e88878c05432947018f1cb0340d0f09e7ae
Daniel Burnette <dcburnette999@gmail.com>
2014-08-28 17:13:33 -0500

    completed O(n) set1 exercises, passed all tests

--- set1/set1.rb
+++ set1/set1.rb
@@ -1,10 +1,74 @@
 module Set1
+
+  #O(n) method
   def self.swap_small(array)
+
+    if array.size == 1
+      return array
+    end
+
+    min = 9999999999999
+    index = 0
+
+    array.each_with_index do |x, i|
+      if x < min 
+        min = x
+        index = i
+      end
+    end
+
+    smallest_val = array[index]
+    first_val = array[0]
+
+    array[0] = smallest_val
+    array[index] = first_val
+    return array
+
   end
 
+  #O(n^2) method
   def self.find_sum_2(array, sum = 0)
+
+    if array.empty?
+      return false
+    end
+
+    array.each do |x|
+      array.each do |y|
+        if (x + y == sum || x == 0)
+          return true
+        end
+      end
+    end
+    return false
   end
 
   def self.find_sum_3(array)
+    
+    if (array.size == 1 && array[0]==0)
+      return true
+    end
+
+    if array.empty?
+      return false
+    end
+
+    array.each do |x|
+      array.each do |y|
+        array.each do |z|
+          if (x==0)
+            return true
+          end
+          if x + y + z == 0
+            #count +=1
+            return true
+          end
+        end
+      end
+    end
+    false
   end
+
 end
+
+
