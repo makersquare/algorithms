53b2dc1f37198d1e876ee4df9cda36527da94b7b
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-08-28 16:55:49 -0500

    Swap small test complete

--- set1/set1.rb
+++ set1/set1.rb
@@ -1,10 +1,26 @@
 module Set1
+
+  # O(n)+O(1) - finding the smallest touches each element O(n), swapping adds O(1) time
   def self.swap_small(array)
+    return nil if array.empty?
+    min = array[0]
+    store_index = 0
+    array.each_index do |ind| 
+      if array[ind] < min
+        min = array[ind] 
+        store_index = ind
+      end
+    end
+    array[0], array[store_index] = array[store_index], array[0]
+    array
   end
 
+  #O(n2) - have to check each element against each other to find the sum
   def self.find_sum_2(array, sum = 0)
+    
   end
 
+  #O(n2^)
   def self.find_sum_3(array)
   end
 end
