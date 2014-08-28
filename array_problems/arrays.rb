fce31259c19c2dff7f2aa9b217d2938e1134989c
Penkar <jspenkar@gmail.com>
2014-08-28 15:09:30 -0500

    Arrays.rb passes all tests.

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -2,30 +2,28 @@ require 'rubygems'
 require 'pry-byebug'
 
 module ArrayUtil
-def max(array)
-  return "No array provided." if array.length == 0
-  highest = array[0]
-  array[1..-1].each {|x| highest = x if highest < x }
-  return highest
-end
-
-
-def middle_element(array)
-  return "No array provided." if array.length == 0
-  length = array.sort!.length
-  p array
-  p length
-  if length % 2 == 0
-    return (array[length/2-1] + array[length/2])/2.0
-  else
-    return array[length/2]
+  def self.max(array)
+    return nil if array.length == 0
+    highest = array[0]
+    array[1..-1].each {|x| highest = x if highest < x }
+    return highest
   end
-end
 
-def sum_arrays(array1, array2)
-  arr = []
-  l = array1.length
-  0.upto(l-1) {|x| arr[x] = array1[x]+array2[x]}
-  return arr
-end
+
+  def self.middle_element(array)
+    return nil if array.length == 0
+    length = array.sort!.length
+    if length % 2 == 0
+      return (array[length/2-1] + array[length/2])/2.0
+    else
+      return array[length/2]
+    end
+  end
+
+  def self.sum_arrays(array1, array2)
+    arr = []
+    l = array1.length
+    0.upto(l-1) {|x| arr[x] = array1[x]+array2[x]}
+    return arr
+  end
 end
