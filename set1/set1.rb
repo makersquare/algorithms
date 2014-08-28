793416ba210f388fe0b8ceac37a4090384cf9227
Josh Dirkx <joshdirkx@gmail.com>
2014-08-28 17:15:24 -0500

    completed algorithm problems and adjusted arrays.rb. all files working via rspec

--- set1/set1.rb
+++ set1/set1.rb
@@ -1,10 +1,55 @@
 module Set1
   def self.swap_small(array)
+    min_value = array[0]
+    min_value_index = 0
+
+    array.each_index do |i|
+      if array[i] < min_value
+        min_value = array[i]
+        min_value_index = i
+      end
+      array[min_value_index] = array[0]
+      array[0] = min_value
+    end
   end
 
   def self.find_sum_2(array, sum = 0)
+    if array.length == 0
+      return false
+    end
+
+    array.each_index do |i|
+      array.each_index do |x|
+        if array[i] + array[x] == 0
+          return true
+        elsif array[i] == 0 || array[x] == 0
+          return true
+        # else 
+          # return false
+        end
+      end
+    end
+    false
   end
 
   def self.find_sum_3(array)
+    if array.length == 0
+      return false
+    end
+
+    array.each_index do |i|
+      array.each_index do |x|
+        array.each_index do |r|
+          if array[i] + array[x] + array[r] == 0
+            return true
+          elsif array[i] == 0 || array[x] == 0 || array[r] == 0
+            return true
+          # else
+          #   return false 
+          end
+        end
+      end
+    end
+    false
   end
 end
