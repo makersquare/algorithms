e819853fe0b26c7772c591343a1e78927aa0255f
Alexandria A. Johnson <alexandria.abdallah@gmail.com>
2014-08-28 18:09:54 -0500

    Defines find_sum_2 and find_sum_3 methods to determine if any of the elements in the array add up 0

--- set1/set1.rb
+++ set1/set1.rb
@@ -16,8 +16,44 @@ module Set1
 end
 
   def self.find_sum_2(array, sum = 0)
+    if array.include?(sum)
+      return true
+    elsif array.empty?
+      return false
+    else
+      array.each_index do |x|
+        array.each_index do |y|
+          if array[x] + array[y] == 0
+            return true
+          end
+        end
+      end
+    end
+    false
   end
 
   def self.find_sum_3(array)
+   if array.include?(0)
+      return true
+    elsif array.empty?
+      return false
+    else
+      array.each_index do |x|
+        array.each_index do |y|
+          array.each_index do |z|
+          if array[x] + array[y] + array[z] == 0
+            return true
+          end
+          end
+        end
+      end
+    end
+    false
   end
+
 end
+
+
+
+
+
