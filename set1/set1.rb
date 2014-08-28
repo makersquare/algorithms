924e95266bd7d24d2f12e810c96e9139874b39d4
Jimmy Pocock <jimmypocock@yahoo.com>
2014-08-28 16:46:22 -0500

    Created self.swap_small method for Set1. All tests pass.

--- set1/set1.rb
+++ set1/set1.rb
@@ -1,5 +1,23 @@
 module Set1
   def self.swap_small(array)
+    first_number = array.first
+    smallest_number = nil
+    position = nil
+    (0...array.length).each do |num|
+      if smallest_number == nil || smallest_number > array[num]
+        smallest_number = array[num]
+        position = num
+      end
+    end
+    if smallest_number != first_number
+      array.delete(smallest_number)
+      array.delete(first_number)
+      array.unshift(smallest_number)
+      array.insert(position, first_number)
+    else
+      array.delete(smallest_number)
+      array.unshift(smallest_number)
+    end
   end
 
   def self.find_sum_2(array, sum = 0)
