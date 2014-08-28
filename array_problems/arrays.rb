61148248e94c41b479e93ee23297bd056c74c96f
Yusef Ouda <yiouda92@gmail.com>
2014-08-28 14:57:04 -0500

    Added max method to ArrayUtil

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -3,6 +3,15 @@ require 'pry-byebug'
 
 module ArrayUtil
   def self.max(array)
+    if array.count > 0  
+      max = array[0]
+      array.each_index do |index|
+        if array[index] > max
+          max = array[index]
+        end
+      end
+      max
+    end
   end
 
   def self.middle_element(array)
