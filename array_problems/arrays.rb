12cb85706c272813b0458ee43224883c5b157787
Nick Damiano <damiano.nick@gmail.com>
2014-08-28 15:32:36 -0500

    solved first problem

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -3,9 +3,23 @@ require 'pry-byebug'
 
 module ArrayUtil
   def self.max(array)
+    biggest= array[0]
+    array.each_with_index do |x, i| 
+      if array[(i+1)] > biggest
+        biggest = x
+        binding.pry
+      end
+    end
+    biggest
   end
 
   def self.middle_element(array)
+    if array.length % 2 == 0
+      #array is even length
+      array.each_with_index do |x,i|
+        
+      end
+
   end
 
   def self.sum_arrays(array1, array2)
