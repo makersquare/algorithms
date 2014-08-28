bacebd79dd9417fc2bed70732726b58b3efee285
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-08-28 15:16:29 -0500

    Edit max method to not use the .sort method and to use the .each method by checking to see if each element is larger than the previous and retaining the higher value.

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -3,12 +3,21 @@ require 'pry-byebug'
 
 module ArrayUtil
   def self.max(array)
-      array.sort!
-      array.last
+    curr_max = array[0]
+    array.each do |x|
+      if x > curr_max
+        curr_max = x
+      end
+    end
+    curr_max
   end
 
   def self.middle_element(array)
-
+    array.sort!
+    # length = array.length
+    # if length.is_odd?
+    #   array[(length - 1)/2]
+    # end
   end
 
   def self.sum_arrays(array1, array2)
