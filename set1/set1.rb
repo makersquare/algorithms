6f89ff7bf1dba226f511f7d13141b22ac9808f44
Nick Damiano <damiano.nick@gmail.com>
2014-08-28 18:47:54 -0500

    fucking butchered the hell out of it but the first problem is done

--- set1/set1.rb
+++ set1/set1.rb
@@ -1,10 +1,35 @@
 module Set1
-  def self.swap_small(array)
+  def self.swap_small(array) #darrell & Jimmy
+    if array.count == 1
+      return array
+    end
+    flag = false
+    smallest_number = array[0]
+    smallest_index = 0
+    array.each_with_index do |value, index|
+      if value < smallest_number
+        smallest_number = value
+        smallest_index = index
+        flag = true
+      end
+    end
+    if !flag
+      return array
+    end
+    array.delete_at(smallest_index)
+    temp = array[0]
+    array.delete(array[0])
+    array.unshift(smallest_number)
+    array.insert(smallest_index, temp)
+    array
   end
 
   def self.find_sum_2(array, sum = 0)
+
   end
 
   def self.find_sum_3(array)
   end
 end
+
+
