9ae54f79b6f3f6730f5113dcfa51fa1e6f208153
Alexandria A. Johnson <alexandria.abdallah@gmail.com>
2014-08-28 15:30:16 -0500

    Max method rewritten to avoid sort method; array middle method defined

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -3,13 +3,25 @@ require 'pry-byebug'
 
 module ArrayUtil
   def self.max(array)
-    array.sort!
-    array[-1]
+    #I found this solution in the Pry Debugger notes. I think it would take some time to execute because I assume it goes through every item in the array, since it's an inject. 
+    array.inject {|y, x| x > y ? x : y }
   end
 
   def self.middle_element(array)
+    #I don't think these would require too much time because the if/else is based on the array length, which is pre-defined in the array.
+    if array.length == 0
+      nil
+    elsif array.length % 2 == 0
+      mid = array.length/2
+      (array[mid] + array[mid - 1])/2.to_f
+    else
+    mid = array.length/2
+    array[mid] 
+    end
   end
 
   def self.sum_arrays(array1, array2)
+    # array1.each_index do |x|
+    #   array1[x]
   end
 end
