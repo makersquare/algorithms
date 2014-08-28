5af031ecd7f4c03f391444392f5a86c9b62821c8
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-08-28 15:30:39 -0500

    Array problems pass, after looking at Patrick's solutions

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -5,23 +5,20 @@ module ArrayUtil
   def self.max(array)
     return nil if array.empty?
     max = array[0]
-    array.each_index { |ind| max > array[ind] ? max : max = array[ind] }
+    array.each { |num| max = num if num > max}
     max
   end
 
   def self.middle_element(array)
     return nil if array.empty?
-    middle = array.length/2
     if array.length.even?
-      result = (array[middle] + array[middle-1])/2.0
+      (array[array.length/2] + array[(array.length/2)-1])/2.0
     else
-      result = array[middle]
+      array[array.length/2]
     end
-    result
   end
 
   def self.sum_arrays(array1, array2)
-    return [] if array1.empty? && array2.empty?
     summed_array = []
     array1.each_index { |ind| summed_array[ind] = array1[ind] + array2[ind] }
     summed_array
