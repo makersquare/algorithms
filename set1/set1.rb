c62936122486d7ce4dea355f8cd4d02cdccbe3db
Alexandria A. Johnson <alexandria.abdallah@gmail.com>
2014-08-28 17:37:03 -0500

    Defines swap_small method in Set1 module

--- set1/set1.rb
+++ set1/set1.rb
@@ -1,6 +1,19 @@
 module Set1
   def self.swap_small(array)
+    if array.length == 1
+      return array
+    else
+      min = array.first
+      array.each do |n|
+        if n < min 
+          min = n
+        end
+    end
+    array[array.index(min)] = array.first
+    array[0] = min
+    array
   end
+end
 
   def self.find_sum_2(array, sum = 0)
   end
