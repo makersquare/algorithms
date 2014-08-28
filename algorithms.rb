4cecc666273501542d9014cfae8c92b0aeb28f4f
Jeffrey Penkar <jspenkar@gmail.com>
2014-08-27 23:07:24 -0500

    middle element created

--- algorithms.rb
+++ algorithms.rb
@@ -6,3 +6,14 @@ def max(array)
 end
 
 
+def middle_element(array)
+	return "No array provided." if array.length == 0
+	length = array.sort!.length
+	p array
+	p length
+	if length % 2 == 0
+		return (array[length/2-1] + array[length/2])/2.0
+	else
+		return array[length/2]
+	end
+end
