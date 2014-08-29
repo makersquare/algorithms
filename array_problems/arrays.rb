ab668c1e481cf4fd15e7a1ceb6d554c323f8b179
Darrell Abrau <darrellabrau@gmail.com>
2014-08-29 04:05:16 -0500

    completed find sum 3

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -2,12 +2,47 @@ require 'rubygems'
 require 'pry-byebug'
 
 module ArrayUtil
+
+
+
   def self.max(array)
+
+  	x = array[0]
+
+  	array.each_with_index do |item, index| 
+  		if (array[index] > x)
+  			x = array[index]
+  		end
+  	end
+		x
   end
+
+
+
+
+
+
+
 
   def self.middle_element(array)
+  
+  	if array.length % 2 == 1
+  		return array[array.length]
+  	# else
+  	# 	return [array[z], array[(z+1)]]
+  	end
   end
 
+
+
+
+
+
+
+
+
+
   def self.sum_arrays(array1, array2)
+
   end
 end
--- set1/set1.rb
+++ set1/set1.rb
@@ -1,10 +1,52 @@
 module Set1
+
+	#O(n)
   def self.swap_small(array)
-  end
+  	first_element=array[0]
 
+  	smallest_value = array[0]
+  	smallest_index = 0
+
+  	array.each_with_index do |item, index|
+  		if array[index] < smallest_value
+  			smallest_value = array[index]
+  			smallest_index = index
+  		end
+  	end
+
+  	array.insert(0, smallest_value)
+  	array.delete_at(1)
+  	array.delete_at(smallest_index)
+  	array.insert(smallest_index, first_element)
+  end
+  #O(n^2)
   def self.find_sum_2(array, sum = 0)
+  	
+
+  	array.each do |x| 
+  		 array.each do |a|
+  		 	if x + a == 0
+  		 		return true
+  		 	end
+  		end
+  	end
+
+  	return false
+
   end
 
+  #O(n^3)
   def self.find_sum_3(array)
+  	array.each do |a|
+  		array.each do |b|
+  			array.each do |c|
+  				if a + b + c == 0
+  					return true
+  				end
+  			end
+  		end
+  	end
+
+  	return false
   end
 end
