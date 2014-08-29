e6dc139db2b7f5615abb61ce2017195587f3b009
Matteo Wiley <matteowiley@gmail.com>
2014-08-28 22:12:45 -0500

    This has almost all of the find sum 2, it is missing the last peice tho compairing the sum.

--- set1/set1.rb
+++ set1/set1.rb
@@ -1,10 +1,38 @@
 module Set1
   def self.swap_small(array)
+    if array.length == 1 
+      return array
+    else
+      min_num = array.first
+      array.each do |x|
+        if x <= min_num
+          min_num = x
+        end
+      end
+      array[array.index(min_num)] = array.first
+      array[0] = min_num
+      array
+    end
   end
 
   def self.find_sum_2(array, sum = 0)
+    if array.length == 0 
+      return false
+    elsif array.include?(0)
+      return true
+    elsif array.each { |x| array.include?(x) && array.include?(-x) }
+      return true
+    else # Can not get this one to work!!!
+      array.each { |x| sum += x }
+      if sum.equ?(0)
+        return false
+      else 
+        return true
+      end
+    end
   end
 
   def self.find_sum_3(array)
-  end
+  end 
+
 end
