49861157765a6a2d1cec71ae424a55f3f5719acd
Penkar <jspenkar@gmail.com>
2014-08-29 09:16:39 -0500

    Slightly updated, stillworks.

--- array_problems/arrays.rb
+++ array_problems/arrays.rb
@@ -3,7 +3,7 @@ require 'pry-byebug'
 
 module ArrayUtil
   def self.max(array)
-    return nil if array.length == 0
+    return nil if array.empty?
     highest = array[0]
     array[1..-1].each {|x| highest = x if highest < x }
     return highest
@@ -11,7 +11,7 @@ module ArrayUtil
 
 
   def self.middle_element(array)
-    return nil if array.length == 0
+    return nil if array.empty?
     length = array.sort!.length
     if length % 2 == 0
       return (array[length/2-1] + array[length/2])/2.0
@@ -22,8 +22,7 @@ module ArrayUtil
 
   def self.sum_arrays(array1, array2)
     arr = []
-    l = array1.length
-    0.upto(l-1) {|x| arr[x] = array1[x]+array2[x]}
+    0.upto(array1.length-1) {|x| arr[x] = array1[x]+array2[x]}
     return arr
   end
 end
