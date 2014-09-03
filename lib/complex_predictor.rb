979617209123caf7cf0970b8be3d5d2639adb932
Benjamin Blattberg <benjamin.blattberg@gmail.com>
2014-09-03 14:49:30 -0500

    Erased unnecessary methods,maintained accuracy

--- lib/complex_predictor.rb
+++ lib/complex_predictor.rb
@@ -2,55 +2,21 @@ require_relative 'predictor'
 require 'pry-byebug'
 
 class ComplexPredictor < Predictor
-  # Public: Trains the predictor on books in our dataset. This method is called
-  # before the predict() method is called.
-  #
-  # Returns nothing.
+  
   def train!
     @data = {}
 
     @all_books.each do |category, books|
       @data[category] = {
-        common_words: [],
-        special_words: []
+        common_words: Set.new,
       }
+
       books.each do |_, tokens|
         tokens = good_tokens(tokens)
         histogram = build_histogram(tokens)
-
         commons = common_words(histogram)
         commons.each { |word| @data[category][:common_words] << word }
-
-        specials = special_words(histogram)
-        specials.each { |word| @data[category][:specials_words] << word }
-      end
-    end
-    uniquify
-    kill_doubles
-  end
-
-  def uniquify
-    @data.each do |category,_|
-      @data[category][:common_words] = @data[category][:common_words].uniq
-    end
-  end
-
-  def kill_doubles
-    kill_list = {}
-    @data.each do |category, _|
-      kill_list[category] = []
-      @data.each do |categories, _|
-        if categories != category
-          @data[categories][:common_words].each do |word|
-            kill_list[category] << word
-          end
-        end
-      end
-    end
-    @data.each do |category, _|
-      @data[category][:common_words].each do |word|
-        @data[category][:common_words].delete(word) if kill_list.include?(word)
-      end
+      end    
     end
   end
 
@@ -72,33 +38,19 @@ class ComplexPredictor < Predictor
     # alternate for most common words
   end
 
-  def special_words(histo)
-    histo.keys.take_while {|word| word.length > 10}
-    # alternate length
-  end
-
-  # Public: Predicts category.
-  #
-  # tokens - A list of tokens (words).
-  #
-  # Returns a category.
   def predict(tokens)
-    tokens = good_tokens(tokens)
-    histogram = build_histogram(tokens)
-    commons = common_words(histogram)
-    specials = special_words(histogram)
-
-    probably_category = nil
+    book_tokens = good_tokens(tokens)
+    book_histogram = build_histogram(book_tokens)
+    book_commons = common_words(book_histogram)
+ 
     max_count = 0
-
+    probably_category = nil
+    
     @data.each do |category, _|
       point_counter = 0
-      commons.each do |word|
+      book_commons.each do |word|
         point_counter += 1 if @data[category][:common_words].include?(word)
       end
-      # specials.each do |word|
-      #   point_counter += 1 if @data[category][:special_words].include?(word)
-      # end
       if point_counter > max_count
         max_count = point_counter
         probably_category = category
