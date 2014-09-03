4dba961fd8c2003c3b351fd6d0d913970e8af574
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2014-09-03 11:55:24 -0500

    Create train! method for complex predictor to take in total words, total books, and top words for each book.

--- lib/complex_predictor.rb
+++ lib/complex_predictor.rb
@@ -7,6 +7,39 @@ class ComplexPredictor < Predictor
   # Returns nothing.
   def train!
     @data = {}
+
+    @all_books.each do |category, books|
+      @data[category] = {
+        words: 0,
+        books: 0,
+        top_words: []
+      }
+      books.each do |filename, tokens|
+        @data[category][:words] += tokens.count
+        @data[category][:books] += 1
+        cutoff_val = good_token_count(tokens).values.sort[-10]
+        @data[category][:top_words] = @good_token_count.select{|k,v| v>= cutoff_val}
+      end
+    end
+
+
+  end
+
+  def good_token_count(tokens)
+    @good_token_count = {
+      #token1: 70,
+      #token2: 50
+    }
+
+    tokens.each do |t|
+      if good_token?(t) && @good_token_count[t] == nil
+        @good_token_count[t] = 1
+      elsif good_token?(t) && @good_token_count[t]
+        @good_token_count[t] += 1
+      end
+    end
+
+    @good_token_count
   end
 
   # Public: Predicts category.
