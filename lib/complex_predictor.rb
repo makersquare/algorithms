652a28c4c3e42a33fbe9e97e30dfb8ed4d26fab5
Nick Damiano <damiano.nick@gmail.com>
2014-09-03 23:58:57 -0500

    added code to complex predictor to sort each book by top 100 frequently used words and compare that to the unknown books to see category. Right now I have two arrays of words and I'm going to subtract them and then see which count is the smallest. the smallest count is the one with the most matching words and the right category.

--- lib/complex_predictor.rb
+++ lib/complex_predictor.rb
@@ -1,12 +1,34 @@
 require_relative 'predictor'
+require 'pry-byebug'
 
 class ComplexPredictor < Predictor
   # Public: Trains the predictor on books in our dataset. This method is called
   # before the predict() method is called.
   #
   # Returns nothing.
+  #This algorithm gets the top 100 most frequent words above 7 letters that occur across all books
+  #for the given subject. Next, it compares the words
   def train!
-    @data = {}
+    @data= {}
+    @words = Hash.new(0)
+    @all_books.each do |category, books|
+      books.each do |filename, words|
+        words.each do |word|
+          if word.length > 6
+            @words[word] += 1
+          end
+        end
+        words_freq = @words.sort_by{|key, value| value}[-100..-1]
+        words_only = words_freq.map{|pair| pair[0]}
+        @data[category] = words_only
+      end
+    end
+    binding.pry 
+    puts "stop"
+#create a hash with the word frequency for each one
+#check to see how many top 20 words match. just like the min one, the closest match or the one with the most
+#similar terms gets selected
+
   end
 
   # Public: Predicts category.
@@ -15,6 +37,31 @@ class ComplexPredictor < Predictor
   #
   # Returns a category.
   def predict(tokens)
+    minimum_category = nil
+    minimum_difference = 999999999
+    @token_words = Hash.new(0)
+    tokens.each do |token|
+      if token.length > 6
+        @token_words[token] +=1
+      end
+    end
+    sorted_token_words = @token_words.sort_by{|key, value| value}[-100..-1]
+    only_words = sorted_token_words.map{|pair| pair[0]}
+    @difference = nil
+    @data.each do |category, word_list| 
+      
+    end
+        minimum_category = category
+        minimum_distance = difference
+      end
+
+    binding.pry
+    #compare sorted_token_words to each @data[:category] hash to see how many words match
+    #calculate matching words or missing words number and store that as minimum difference
+    #store the subject as minimum subject
+    #do the next category and if the minimum difference is larger swap out values
+    #return the minimum subject
+    #
     # Always predict astronomy, for now.
     :astronomy
   end
