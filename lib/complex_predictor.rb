98dfbcc49deee6eca8deb1359a9d3bbe17ebd017
Matteo Wiley <matteowiley@gmail.com>
2014-09-04 10:43:46 -0500

    This takes it up too 100% and down to 5 secs

--- lib/complex_predictor.rb
+++ lib/complex_predictor.rb
@@ -18,17 +18,21 @@ class ComplexPredictor < Predictor
 
       books.each do |filename, token|
         token.each do |word|
-          if good_token?(word) && word.length > 5
+          if good_token?(word) && word.length > 7
             @data[category][:uniq_words][word] += 1
           end
         end
         @author_start = token.find_index("by")
         @data[category][:authors] << token[(@author_start + 1)..(@author_start+2)]
         end
-        @data[category][:uniq_words] = @data[category][:uniq_words].sort_by {|key, value| value}.reverse[0..14]
+        @new_hash = Hash.new(0)
+        @most_pop = @data[category][:uniq_words].sort_by {|key, value| value}.reverse[0..100]
+        @most_pop.map! {|word, count| word}
+        # @most_pop.each do |x|
+        #   @new_hash[x.first] = x.last
+        # end
+        @data[category][:uniq_words] = @most_pop
         
-        # binding.pry
-     
     end
     
 
@@ -47,17 +51,11 @@ class ComplexPredictor < Predictor
   # Returns a category.
   def predict(tokens)
     @match = Hash.new(0)
-    
     @data.each do |category, uniq|
-      tokens.each do |word|
-        uniq[:uniq_words].each do |uni_w|
-          if uni_w.include?(word)
-            @match[category] += 1
-          end
-        end
-      end
+      @match[category] = (@data[category][:uniq_words] & tokens).length
     end
     @match.max_by {|category, count| count }.first
-  end
+   end
+
 end
 
