08bfbc8ee6a14133f202cdb4e4ae837bbe9087ef
Matteo Wiley <matteowiley@gmail.com>
2014-09-03 21:08:48 -0500

    This organizes the data into unique words and author

--- lib/complex_predictor.rb
+++ lib/complex_predictor.rb
@@ -1,4 +1,5 @@
 require_relative 'predictor'
+require 'pry-byebug'
 
 class ComplexPredictor < Predictor
   # Public: Trains the predictor on books in our dataset. This method is called
@@ -6,7 +7,34 @@ class ComplexPredictor < Predictor
   #
   # Returns nothing.
   def train!
+    
     @data = {}
+
+    @all_books.each do |category, books|
+      @data[category] = {
+        uniq_words: Hash.new(0),
+        authors: []
+      }
+
+      books.each do |filename, token|
+        token.each do |word|
+          if good_token?(word) && word.length > 5
+            @data[category][:uniq_words][word] += 1
+          end
+        @author_start = token.find_index("by")
+        @data[category][:authors] << token[(@author_start + 1)..(@author_start+2)]
+        end
+      end
+      @data[category][:uniq_words] = @data[category][:uniq_words].sort_by {|key, value| value}.reverse[0..14]
+      binding.pry
+    end
+
+     #@date = {
+     #unq_word: []
+     #book:
+     # 
+     #
+     #}
   end
 
   # Public: Predicts category.
