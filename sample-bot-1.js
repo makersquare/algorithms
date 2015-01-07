ab2a4aa1b212b4942794b2a64cf70a38a3ea7709
Gilbert <gilbertbgarza@gmail.com>
2015-01-07 08:44:38 -0600

    More robust bot system

--- sample-bot-1.js
+++ sample-bot-1.js
@@ -1,15 +1,22 @@
 (function () {
 
-  var bot = {
-    name: 'dummy-bot',
-    play: function (drawnCard, remainingDeckSize, moveType) {
-      // moveType will be either 'normal', 'war', 'normal-gamble', or 'war-gamble'
-      return 'accept'
-    },
-    handleRoundResult: function (didIWin, loot) {
-      // TODO: Whatever you want. You can do aanything
+  var botName = 'dummy-bot'
+
+  var BotClass = function () {
+    // <<Initialize bot state here>>
+
+    return {
+      name: botName,
+      play: function (drawnCard, remainingDeckSize, moveType) {
+        // moveType will be either 'normal', 'war', 'normal-gamble', or 'war-gamble'
+        return 'accept'
+      },
+      handleRoundResult: function (didIWin, loot) {
+        // TODO: Whatever you want. You can do aanything
+      }
     }
   }
 
-  registerBot(bot)
+  BotClass.botName = botName
+  registerBot(botName, BotClass)
 })()
--- sample-bot-2.js
+++ sample-bot-2.js
@@ -1,20 +1,27 @@
 (function () {
 
-  var bot = {
-    name: 'reckless-bot',
-    play: function (drawnCard, remainingDeckSize, moveType) {
-      // moveType will be either 'normal', 'war', 'normal-gamble', or 'war-gamble'
-      if (moveType === 'normal' || moveType === 'war') {
-        return 'gamble'
+  var botName = 'reckless-bot'
+
+  var BotClass = function () {
+    // <<Initialize bot state here>>
+
+    return {
+      name: botName,
+      play: function (drawnCard, remainingDeckSize, moveType) {
+        // moveType will be either 'normal', 'war', 'normal-gamble', or 'war-gamble'
+        if (moveType === 'normal' || moveType === 'war') {
+          return 'gamble'
+        }
+        else {
+          return 'accept'
+        }
+      },
+      handleRoundResult: function (didIWin, loot) {
+        // TODO: Whatever you want. You can do aanything
       }
-      else {
-        return 'accept'
-      }
-    },
-    handleRoundResult: function (didIWin, loot) {
-      // TODO: Whatever you want. You can do aanything
     }
   }
 
-  registerBot(bot)
+  BotClass.botName = botName
+  registerBot(botName, BotClass)
 })()
